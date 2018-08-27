80be8c2feb7d689adbc230fdeb9d905e3be961b0
Markus Winter <36643508+dev-braini@users.noreply.github.com>
2018-08-27 18:31:46 +0200

    2

---
+++ src/main/java/skinsrestorer/bungee/SkinsRestorer.java
@@ -0,0 +1,141 @@
+package skinsrestorer.bungee;
+
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.plugin.Plugin;
+//import org.bstats.bungeecord.MetricsLite;
+import skinsrestorer.bungee.commands.AdminCommands;
+import skinsrestorer.bungee.commands.PlayerCommands;
+import skinsrestorer.bungee.listeners.LoginListener;
+import skinsrestorer.shared.storage.Config;
+import skinsrestorer.shared.storage.Locale;
+import skinsrestorer.shared.storage.SkinStorage;
+import skinsrestorer.shared.utils.MojangAPI;
+import skinsrestorer.shared.utils.MojangAPI.SkinRequestException;
+import skinsrestorer.shared.utils.MySQL;
+
+import javax.net.ssl.HttpsURLConnection;
+import java.io.*;
+import java.net.URL;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class SkinsRestorer extends Plugin {
+
+    private static SkinsRestorer instance;
+    private MySQL mysql;
+    private boolean multibungee;
+    private ExecutorService exe;
+    private boolean outdated;
+
+    public static SkinsRestorer getInstance() {
+        return instance;
+    }
+
+    public String checkVersion(CommandSender console) {
+        try {
+            HttpsURLConnection con = (HttpsURLConnection) new URL("https://api.spigotmc.org/legacy/update.php?resource=2124")
+                    .openConnection();
+            con.setDoOutput(true);
+            con.setRequestMethod("GET");
+            String version = new BufferedReader(new InputStreamReader(con.getInputStream())).readLine();
+            if (version.length() <= 13)
+                return version;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            console.sendMessage(new TextComponent("§e[§2SkinsRestorer§e] §cFailed to check for an update on Spigot."));
+        }
+        return getVersion();
+    }
+
+    public ExecutorService getExecutor() {
+        return exe;
+    }
+
+    public MySQL getMySQL() {
+        return mysql;
+    }
+
+    public String getVersion() {
+        return getDescription().getVersion();
+    }
+
+    public boolean isMultiBungee() {
+        return multibungee;
+    }
+
+    public boolean isOutdated() {
+        return outdated;
+    }
+
+    @Override
+    public void onEnable() {
+
+        //@SuppressWarnings("unused")
+        //MetricsLite metrics = new MetricsLite(this);
+
+        instance = this;
+        Config.load(getResourceAsStream("config.yml"));
+        Locale.load();
+        exe = Executors.newCachedThreadPool();
+
+        if (Config.USE_MYSQL)
+            SkinStorage.init(mysql = new MySQL(Config.MYSQL_HOST, Config.MYSQL_PORT, Config.MYSQL_DATABASE,
+                    Config.MYSQL_USERNAME, Config.MYSQL_PASSWORD));
+        else
+            SkinStorage.init(getDataFolder());
+
+        getProxy().getPluginManager().registerListener(this, new LoginListener());
+        getProxy().getPluginManager().registerCommand(this, new AdminCommands());
+        getProxy().getPluginManager().registerCommand(this, new PlayerCommands());
+        getProxy().registerChannel("SkinsRestorer");
+        SkinApplier.init();
+
+        multibungee = Config.MULTIBUNGEE_ENABLED
+                || ProxyServer.getInstance().getPluginManager().getPlugin("RedisBungee") != null;
+
+        exe.submit(new Runnable() {
+
+            @Override
+            public void run() {
+
+                CommandSender console = getProxy().getConsole();
+
+                if (Config.UPDATER_ENABLED)
+                    if (checkVersion(console).equals(getVersion())) {
+                        outdated = false;
+                        console.sendMessage(new TextComponent("§e[§2SkinsRestorer§e] §a----------------------------------------------"));
+                        console.sendMessage(new TextComponent("§e[§2SkinsRestorer§e] §a    +===============+"));
+                        console.sendMessage(new TextComponent("§e[§2SkinsRestorer§e] §a    | SkinsRestorer |"));
+                        console.sendMessage(new TextComponent("§e[§2SkinsRestorer§e] §a    +===============+"));
+                        console.sendMessage(new TextComponent("§e[§2SkinsRestorer§e] §a----------------------------------------------"));
+                        console.sendMessage(new TextComponent("§e[§2SkinsRestorer§e] §b    Current version: §a" + getVersion()));
+                        console.sendMessage(new TextComponent("§e[§2SkinsRestorer§e] §a    This is the latest version!"));
+                        console.sendMessage(new TextComponent("§e[§2SkinsRestorer§e] §a----------------------------------------------"));
+                    } else {
+                        outdated = true;
+                        console.sendMessage(new TextComponent("§e[§2SkinsRestorer§e] §a----------------------------------------------"));
+                        console.sendMessage(new TextComponent("§e[§2SkinsRestorer§e] §a    +===============+"));
+                        console.sendMessage(new TextComponent("§e[§2SkinsRestorer§e] §a    | SkinsRestorer |"));
+                        console.sendMessage(new TextComponent("§e[§2SkinsRestorer§e] §a    +===============+"));
+                        console.sendMessage(new TextComponent("§e[§2SkinsRestorer§e] §a----------------------------------------------"));
+                        console.sendMessage(new TextComponent("§e[§2SkinsRestorer§e] §b    Current version: §c" + getVersion()));
+                        console.sendMessage(new TextComponent("§e[§2SkinsRestorer§e] §e    A new version is available! Download it at:"));
+                        console.sendMessage(new TextComponent("§e[§2SkinsRestorer§e] §e    https://www.spigotmc.org/resources/skinsrestorer.2124"));
+                        console.sendMessage(new TextComponent("§e[§2SkinsRestorer§e] §a----------------------------------------------"));
+                    }
+
+                if (Config.DEFAULT_SKINS_ENABLED)
+                    for (String skin : Config.DEFAULT_SKINS)
+                        try {
+                            SkinStorage.setSkinData(skin, MojangAPI.getSkinProperty(MojangAPI.getUUID(skin)));
+                        } catch (SkinRequestException e) {
+                            if (SkinStorage.getSkinData(skin) == null)
+                                console.sendMessage(new TextComponent("§e[§2SkinsRestorer§e] §cDefault Skin '" + skin + "' request error:" + e.getReason()));
+                        }
+            }
+
+        });
+    }
+}
---
+++ src/main/java/skinsrestorer/bungee/listeners/LoginListener.java
@@ -0,0 +1,63 @@
+package skinsrestorer.bungee.listeners;
+
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.config.ServerInfo;
+import net.md_5.bungee.api.connection.PendingConnection;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.md_5.bungee.api.connection.Server;
+import net.md_5.bungee.api.event.LoginEvent;
+import net.md_5.bungee.api.event.PlayerDisconnectEvent;
+import net.md_5.bungee.api.event.PostLoginEvent;
+import net.md_5.bungee.api.event.ServerConnectEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.event.EventPriority;
+import skinsrestorer.bungee.SkinApplier;
+import skinsrestorer.bungee.SkinsRestorer;
+import skinsrestorer.shared.storage.Config;
+import skinsrestorer.shared.storage.Locale;
+import skinsrestorer.shared.storage.SkinStorage;
+import skinsrestorer.shared.utils.C;
+import skinsrestorer.shared.utils.MojangAPI;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+public class LoginListener implements Listener {
+
+    @EventHandler (priority = EventPriority.HIGHEST)
+    public void onServerConnect(ServerConnectEvent e) {
+        ProxyServer.getInstance().getScheduler().runAsync(SkinsRestorer.getInstance(), () -> {
+            if (Config.UPDATER_ENABLED && SkinsRestorer.getInstance().isOutdated()
+                    && e.getPlayer().hasPermission("skinsrestorer.cmds"))
+                e.getPlayer().sendMessage(new TextComponent(C.c(Locale.OUTDATED)));
+
+            if (Config.DISABLE_ONJOIN_SKINS)
+                return;
+
+            if (Config.DEFAULT_SKINS_ENABLED) {
+                try {
+                    List<String> skins = Config.DEFAULT_SKINS;
+                    int randomNum = 0 + (int) (Math.random() * skins.size());
+                    SkinStorage.getOrCreateSkinForPlayer(e.getPlayer().getName());
+                    SkinStorage.setPlayerSkin(e.getPlayer().getName(), skins.get(randomNum));
+                    SkinApplier.applySkin(e.getPlayer().getName());
+                    return;
+                } catch (MojangAPI.SkinRequestException ex) {
+                    ex.printStackTrace();
+                }
+            }
+
+            SkinsRestorer.getInstance().getProxy().getScheduler().schedule(SkinsRestorer.getInstance(), new Runnable() {
+
+                @Override
+                public void run() {
+                    SkinApplier.applySkin(e.getPlayer());
+                }
+            }, 2000, TimeUnit.MILLISECONDS);
+        });
+    }
+}
